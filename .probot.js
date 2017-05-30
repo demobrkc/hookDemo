949f624301d5e6aba86b3c8005973c456afd8f4c
Philip Holleran <pholleran@users.noreply.github.com>
2017-05-30 13:54:59 -0500

    Update .probot.js

--- .probot.js
+++ .probot.js
@@ -1,6 +1,3 @@
 on('issues.opened')
    .comment(`
    Hello @{{ sender.login }}. Thanks for inviting me to your project.`)
-  // .createCard({project:'myProject', column:'New'})
-   
- include("probot/workflow:docs/demo.js");
