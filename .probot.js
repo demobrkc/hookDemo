cc257480ee3e1cc3875e560171927ecc5e1d0586
Philip Holleran <pholleran@users.noreply.github.com>
2017-05-30 13:49:53 -0500

    Update .probot.js

--- .probot.js
+++ .probot.js
@@ -1,11 +1,6 @@
 on('issues.opened')
    .comment(`
-   Hello @{{ sender.login }}. Thanks for inviting me to your project.
-   Read more about [all the things I can help you with][config]. I can't
-   wait to get started!
-
-   [config]: https://github.com/bkeepers/PRobot/blob/master/docs/configuration.md
- `)
+   Hello @{{ sender.login }}. Thanks for inviting me to your project.`)
   // .createCard({project:'myProject', column:'New'})
    
  include("probot/workflow:docs/demo.js");
